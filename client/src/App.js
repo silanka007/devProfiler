22bdddac6d1725a26bded99bf060665bdcf69d93
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2020-06-02 22:11:46 +0100

    creation of navbar and landing component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Navbar from './components/layouts/navbar';
+import Landing from './components/layouts/landing';
+
 function App() {
   return (
     <>
-     <h1>hello from silanka</h1>
+      <Navbar />
+      <Landing />
     </>
   );
 }
